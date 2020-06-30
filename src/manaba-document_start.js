4c32b12cf3a57289b68bf1de7f803417e86ef5eb
mkobayashime <mkobayashime@gmail.com>
2020-06-30 16:33:34 +0900

    [Add] Add createSearchbox.js

--- src/manaba-document_start.js
+++ src/manaba-document_start.js
@@ -3,8 +3,10 @@
 import removeLinkBalloon from "./methods/removeLinkBalloon.js"
 import checkPagePubDeadline from "./methods/checkPagePubDeadline.js"
 import checkAssignmentDeadline from "./methods/checkAssignmentDeadline.js"
+import createSearchbox from "./methods/createSearchbox.js"
 
 window.addEventListener("DOMContentLoaded", () => {
+  const url = window.location.href
   removeLinkBalloon()
 
   const pageLimitView = document.getElementsByClassName("pagelimitview")[0]
@@ -15,5 +17,9 @@ window.addEventListener("DOMContentLoaded", () => {
   const stdlist = document.getElementsByClassName("stdlist")[0]
   if (stdlist) {
     checkAssignmentDeadline()
+  }
+
+  if (url.lastIndexOf("home") === url.length - 4) {
+    createSearchbox()
   }
 })
