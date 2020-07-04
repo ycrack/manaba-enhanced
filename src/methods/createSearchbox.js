e5bf23b742899c9c8b082e14d8e89a6bd3becfbd
Masaki Kobayashi <mkobayashime@gmail.com>
2020-07-04 16:44:17 +0900

    [Chore] Use Array.map instead of forOf

--- src/methods/createSearchbox.js
+++ src/methods/createSearchbox.js
@@ -5,13 +5,13 @@ import axios from "axios"
 import { gooLabAppId } from "../../.env"
 
 const createSearchbox = async () => {
-  const titleContainers = document.getElementsByClassName("courselist-title")
+  const titleContainers = Array.from(
+    document.getElementsByClassName("courselist-title")
+  )
 
-  const titles = []
-  for (const titleContainer of titleContainers) {
-    const title = titleContainer.innerText
-    titles.push(title)
-  }
+  const titles = titleContainers.map(
+    (titleContainer) => titleContainer.innerText
+  )
 
   const params = new URLSearchParams()
   params.append("app_id", gooLabAppId)
